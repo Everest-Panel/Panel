8f7d2f8a8d3cd82fe3d55c2476b40d76b31a0b1a
Devon Edwards <xXEthyleneXx@gmail.com>
2024-10-17 18:07:52 -0400

    Frontend & CLI
    Scaling frontend correctly aswell as working on the CLI script to handle internal cmds for issue etc.

--- frontend/app/public/index.php
+++ frontend/app/public/index.php
@@ -5,13 +5,39 @@
 <html>
     <head>
         <title>Everest Panel | Login</title>
-        <link rel="stylesheet" type="text/css" href="http://localhost/api/dump/?type=styles&file=default.css">
-        <link rel="stylesheet" type="text/css" href="http://localhost/api/dump/?type=styles&file=login.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="/api/dump/?type=styles&file=default.css">
+        <link rel="stylesheet" type="text/css" href="/api/dump/?type=styles&file=login.css">
     </head>
     <body>
         <div id="login">
             <div class="icon">
                 <img class="icon" alt="Everest Icon" draggable="false" src="http://localhost/api/dump/?type=images&file=Everest_Icon.png">
+            </div>
+            <div class="title">
+                Everest Login
+            </div>
+            <div class="bars">
+                <div class="bar active"></div>
+                <div class="bar"></div>
+            </div>
+            <div class="inputs">
+                <div class="input">
+                    <label for="username">Username</label>
+                    <input id="username" type="text" placeholder="JohnDoe" maxlength="30" autofocus tabindex="1">
+                </div>
+                <div class="input">
+                    <label for="password">Password</label>
+                    <input id="password" type="password" placeholder="SuperSecret" maxlength="254" tabindex="2">
+                </div>
+                <div class="input">
+                    <div class="small">
+                        <div class="text">Forgot Password?</div>
+                    </div>
+                </div>
+                <div class="input">
+                    <div class="submit">Submit</div>
+                </div>
             </div>
         </div>
     </body>
